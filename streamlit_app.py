4fa852c20f1cfd3196bfbb8ce073a022f789c569
quicklearner15 <113660174+quicklearner15@users.noreply.github.com>
2023-01-02 14:26:00 +0530

    Update streamlit_app.py
    name error

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 st.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
 st.dataframe(my_fruit_list)
