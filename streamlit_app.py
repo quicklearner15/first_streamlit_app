4138344b454529b01195da2d085ef5e4ecedea41
quicklearner15 <113660174+quicklearner15@users.noreply.github.com>
2023-01-02 14:20:01 +0530

    Update streamlit_app.py
    Added import pandas to read csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
 st.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -7,5 +8,6 @@ st.text('🐔Hard-Boiled Free-Range Egg')
 st.text('🥑🍞Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
  
